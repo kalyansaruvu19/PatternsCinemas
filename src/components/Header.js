7a27a0c5d82cc6979e4cf32709b4c7ce8061bf58
kalyansaruvu <saruvukalyan19@gmail.com>
2025-05-14 19:57:14 +0530

    style changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -21,7 +21,7 @@ export const Header = () => {
   }, [darkMode]);
 
   const activeClass =
-    "text-base block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white";
+    "text-base block py-2 pr-4 pl-3 text-white bg-green-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white";
   const inActiveClass =
     "text-base block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700";
 
