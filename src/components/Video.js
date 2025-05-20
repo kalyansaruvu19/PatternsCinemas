7edc6efd571b835726eb34be2a4fadbc16d7862c
kalyansaruvu <saruvukalyan19@gmail.com>
2025-05-21 04:50:49 +0530

    changes video

--- src/components/Video.js
+++ src/components/Video.js
@@ -2,13 +2,10 @@ import video from "../video/largevideo.mp4"
 
 export const Video = () => {
   return (
-    <div><section>
-                        
-              <video class="w-full" autoplay controls >
-                <source src={video} type="video/mp4" />
-               
-              </video>
-              
-                      </section></div>
+    <div>
+    <video class="w-full" autoplay controls >
+     <source  src={video} type="video/mp4" />
+    </video>
+    </div>
   )
 }
